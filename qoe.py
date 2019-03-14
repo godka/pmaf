8a19e3e385a429cabb5178cd02219daa1d9dc2c5
godka <htc17@mails.tsinghua.edu.cn>
2019-03-14 20:53:42 +0800

    hi

--- qoe.py
+++ qoe.py
@@ -1,15 +1,41 @@
 import tensorflow as tf
 import tflearn
 import numpy as np
+import h5py
 
 class qoe(object):
-    def __init__(self, sess):
+    def __init__(self, sess, learning_rate):
         self.sess = sess
         self.inputs, self.out = self.create_network()
-        trainer = tf.train.Saver()
-        trainer.restore(self.sess, 'qoe/model.ckpt')
+        self.lr_rate = learning_rate
+        self.outputs = tflearn.input_data(shape=[None, 1], name='output')
+        f = h5py.File("qoe.hdf5", "r")
+        self.x = np.array(f['x'])
+        self.y_ = np.array(f['y'])
+        f.close()
+        # Compute loss functions
+        self.obj = tflearn.objectives.mean_square(self.out, self.outputs)
+        self.batch_size = 64
+        # Optimization Op
+        self.optimize = tf.train.AdamOptimizer(
+            self.lr_rate).minimize(self.obj)
+        #trainer = tf.train.Saver()
+        #trainer.restore(self.sess, 'qoe/model.ckpt')
         #y_ = sess.run(net, feed_dict={inputs: X})
         #alpha_ = sess.run(soft, feed_dict={inputs: X})
+        # Get all network parameters
+        self.network_params = \
+            tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='qoe')
+
+        # Set all network parameters
+        self.input_network_params = []
+        for param in self.network_params:
+            self.input_network_params.append(
+                tf.placeholder(tf.float32, shape=param.get_shape()))
+        self.set_network_params_op = []
+        for idx, param in enumerate(self.input_network_params):
+            self.set_network_params_op.append(
+                self.network_params[idx].assign(param))
 
     def conv_1d_res_block(self, net, filter_num, filter_size):
         _shape = net.get_shape().as_list()
@@ -27,26 +53,42 @@ class qoe(object):
         return out
 
     def create_network(self):
-        inputs = tflearn.input_data(shape=[None, 3], name='input')
-        softnet = tf.expand_dims(inputs, -1)
-        softnet = tflearn.conv_1d(softnet, 64, 1, activation='relu')
-        softnet = tflearn.batch_normalization(softnet)
-        for p in range(20):
-            softnet = self.conv_1d_res_block(softnet, 64, 3)
-        #softnet = tf.expand_dims(softnet, 1)
-        #soft = tflearn.global_avg_pool(softnet)
-        soft = tflearn.fully_connected(softnet, 64, activation='relu')
-        soft = tflearn.fully_connected(soft, 2, activation='linear')
-        soft = tf.expand_dims(soft, axis=-1)
+        with tf.variable_scope('qoe'):
+            inputs = tflearn.input_data(shape=[None, 3], name='input')
+            softnet = tf.expand_dims(inputs, -1)
+            softnet = tflearn.conv_1d(softnet, 64, 1, activation='relu')
+            softnet = tflearn.batch_normalization(softnet)
+            for p in range(20):
+                softnet = self.conv_1d_res_block(softnet, 64, 3)
+            #softnet = tf.expand_dims(softnet, 1)
+            #soft = tflearn.global_avg_pool(softnet)
+            soft = tflearn.fully_connected(softnet, 64, activation='relu')
+            soft = tflearn.fully_connected(soft, 2, activation='linear')
+            soft = tf.expand_dims(soft, axis=-1)
 
-        ret = tf.expand_dims(inputs[:, 1:], 1)
-        net = tf.matmul(ret, soft)
-        net = tf.reshape(net, (-1, 1))
-        net = tf.add(net, inputs[:, 0:1])
-
-        return inputs, net
+            ret = tf.expand_dims(inputs[:, 1:], 1)
+            net = tf.matmul(ret, soft)
+            net = tf.reshape(net, (-1, 1))
+            net = tf.add(net, inputs[:, 0:1])
+            net = tf.nn.sigmoid(net)
+            return inputs, net
 
     def predict(self, inputs):
         y_ = self.sess.run(self.out, feed_dict={
                       self.inputs: np.reshape(inputs, (-1, 3))})
-        return y_[0][0]
+        return y_
+
+    def train(self):
+        self.x, self.y_ = tflearn.data_utils.shuffle(self.x, self.y_)
+        self.sess.run(self.optimize, feed_dict={
+            self.inputs: self.x[:self.batch_size],
+            self.outputs: self.y_[:self.batch_size]
+        })
+
+    def get_network_params(self):
+        return self.sess.run(self.network_params)
+
+    def set_network_params(self, input_network_params):
+        self.sess.run(self.set_network_params_op, feed_dict={
+            i: d for i, d in zip(self.input_network_params, input_network_params)
+        })
--- train.py
+++ train.py
@@ -93,7 +93,7 @@ def central_agent(net_params_queues, exp_queues):
                         level=logging.INFO)
 
     with tf.Session() as sess, open(LOG_FILE + '_test', 'w') as test_log_file:
-
+        qoe_model  =qoe.qoe(sess, learning_rate=ACTOR_LR_RATE)
         actor = a3c.ActorNetwork(sess,
                                  state_dim=[S_INFO, S_LEN], action_dim=A_DIM,
                                  learning_rate=ACTOR_LR_RATE)
@@ -123,9 +123,10 @@ def central_agent(net_params_queues, exp_queues):
             # synchronize the network parameters of work agent
             actor_net_params = actor.get_network_params()
             critic_net_params = critic.get_network_params()
+            qoe_net_params = qoe_model.get_network_params()
             #rew_net_params = rew.get_network_params()
             for i in range(NUM_AGENTS):
-                net_params_queues[i].put([actor_net_params, critic_net_params])
+                net_params_queues[i].put([actor_net_params, critic_net_params, qoe_net_params])
                 # Note: this is synchronous version of the parallel training,
                 # which is easier to understand and probe. The framework can be
                 # fairly easily modified to support asynchronous training.
@@ -235,7 +236,7 @@ def agent(agent_id, all_cooked_time, all_cooked_bw, net_params_queue, exp_queue)
                               random_seed=agent_id)
 
     with tf.Session() as sess, open(LOG_FILE + '_agent_' + str(agent_id), 'w') as log_file:
-        qoe_model = qoe.qoe(sess)
+        qoe_model = qoe.qoe(sess, learning_rate=ACTOR_LR_RATE)
         actor = a3c.ActorNetwork(sess,
                                  state_dim=[S_INFO, S_LEN], action_dim=A_DIM,
                                  learning_rate=ACTOR_LR_RATE)
@@ -245,14 +246,14 @@ def agent(agent_id, all_cooked_time, all_cooked_bw, net_params_queue, exp_queue)
         #rew = disc.DiscNetwork(
         #    sess, state_dim=[4], learning_rate=ACTOR_LR_RATE / 10.)
         # initial synchronization of the network parameters from the coordinator
-        actor_net_params, critic_net_params = net_params_queue.get()
+        actor_net_params, critic_net_params, qoe_net_params = net_params_queue.get()
         actor.set_network_params(actor_net_params)
         critic.set_network_params(critic_net_params)
+        qoe_model.set_network_params(qoe_net_params)
         #rew.set_network_params(rew_net_params)
 
         last_bit_rate = DEFAULT_QUALITY
         bit_rate = DEFAULT_QUALITY
-        mos_on_demand = np.random.uniform(0.3,0.9)
         last_chunk_vmaf = None
 
         action_vec = np.zeros(A_DIM)
@@ -289,7 +290,7 @@ def agent(agent_id, all_cooked_time, all_cooked_bw, net_params_queue, exp_queue)
             d_state[2] = np.abs(video_chunk_vmaf-last_chunk_vmaf) / 100.
             #d_state[3] = mos_on_demand
 
-            reward =  qoe_model.predict(d_state) * 100.
+            reward = qoe_model.predict(d_state) * 100.
             #rew.predict(np.reshape(d_state, (-1, 4)))
             #reward = reward[0, 0]
             #d_batch.append(d_state)
@@ -352,9 +353,10 @@ def agent(agent_id, all_cooked_time, all_cooked_bw, net_params_queue, exp_queue)
                                {'entropy': entropy_record}])
 
                 # synchronize the network parameters from the coordinator
-                actor_net_params, critic_net_params = net_params_queue.get()
+                actor_net_params, critic_net_params, qoe_net_params = net_params_queue.get()
                 actor.set_network_params(actor_net_params)
                 critic.set_network_params(critic_net_params)
+                qoe_model.set_network_params(qoe_net_params)
                 #rew.set_network_params(rew_net_params)
 
                 del s_batch[:]
@@ -370,7 +372,6 @@ def agent(agent_id, all_cooked_time, all_cooked_bw, net_params_queue, exp_queue)
             if end_of_video:
                 last_bit_rate = DEFAULT_QUALITY
                 bit_rate = DEFAULT_QUALITY  # use the default action here
-                mos_on_demand = np.random.uniform(0.3, 0.9)
                 last_chunk_vmaf = None
                 chunk_index = 0
 
